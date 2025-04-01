f1b3434b63916aa753878232e5e2f2b6bc27a283
Brian Cheung <bricheung@umass.edu>
2025-03-31 22:39:44 -0400

    updated bottomNav styling

--- src/components/customer/HomePage/BottomNav.tsx
+++ src/components/customer/HomePage/BottomNav.tsx
@@ -1,13 +1,21 @@
-import '../../../styles/customer/home/BottomNav.css'
 import { NavLink } from 'react-router-dom'
+import '../../../styles/customer/home/BottomNav.css'
 
 const BottomNav = () => {
   return (
     <nav className="bottom-nav">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/rewards">Rewards</NavLink>
-      <NavLink to="/scan">Scan</NavLink>
-      <NavLink to="/profile">Account</NavLink>
+      <NavLink to="/" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+        Home
+      </NavLink>
+      <NavLink to="/rewards" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+        Rewards
+      </NavLink>
+      <NavLink to="/scan" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+        Scan
+      </NavLink>
+      <NavLink to="/profile" className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+        Account
+      </NavLink>
     </nav>
   )
 }
