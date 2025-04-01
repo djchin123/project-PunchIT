1a0a7319a64092d0f48c6c283e5e5ceff02ea8e2
Brian Cheung <bricheung@umass.edu>
2025-03-31 22:43:25 -0400

    added a last visit text

--- src/components/customer/HomePage/HomePageLayout.tsx
+++ src/components/customer/HomePage/HomePageLayout.tsx
@@ -6,6 +6,8 @@ const HomePageLayout = () => {
   return (
     <div className="customer-home">
       <h1 className="greeting">Welcome back, Brian</h1>
+      <p>We miss you! Last scan: March 28, 2025</p>
+
       <PunchCard total={10} filled={4} />
 
       <BottomNav />
